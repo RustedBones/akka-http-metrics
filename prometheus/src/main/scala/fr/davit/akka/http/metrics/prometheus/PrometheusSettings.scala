1797f82c7e5b91d72b4ed933e16910179b76f163
Robert Jacob <robert.jacob@holidaycheck.com>
2020-01-13 20:02:57 +0100

    Update buckets

--- prometheus/src/main/scala/fr/davit/akka/http/metrics/prometheus/PrometheusSettings.scala
+++ prometheus/src/main/scala/fr/davit/akka/http/metrics/prometheus/PrometheusSettings.scala
@@ -24,7 +24,7 @@ package fr.davit.akka.http.metrics.prometheus
   * @param responseSizeBuckets Buckets to use for calculating response size histogram (bytes).
   */
 case class PrometheusSettings(
-    durationBuckets: Seq[Double] = Seq(0.005, 0.01, 0.025, 0.05, 0.1, 0.25, 0.5, 1, 2, 5, 10),
-    requestSizeBuckets: Seq[Double] = Seq(0, 1024, 10240, 102400, 1048576, 10485760),
-    responseSizeBuckets: Seq[Double] = Seq(0, 1024, 10240, 102400, 1048576, 10485760)
+    durationBuckets: Seq[Double] = Seq(0.005, 0.01, 0.025, 0.05, 0.1, 0.25, 0.5, 1, 2.5, 5, 10),
+    requestSizeBuckets: Seq[Double] = Seq(0, 128, 256, 512, 1024, 10240, 102400, 1048576, 10485760),
+    responseSizeBuckets: Seq[Double] = Seq(128, 256, 512, 1024, 10240, 102400, 1048576, 10485760)
 )
