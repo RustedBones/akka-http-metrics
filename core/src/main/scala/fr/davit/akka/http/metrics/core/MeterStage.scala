924cadcb82c1d1ce0e1f983deab53fb11b8d9790
Pieter de Zwart <pdezwart@gmail.com>
2020-11-06 01:08:22 -0800

    Fixing One2OneBidi flow exception when request is cancelled (#68)
    * Issue #67: Fix OutputTruncationException by properly handling Bidi flow
    
    * BidiFlow independent termination
    
    * Header and format
    
    Co-authored-by: Michel Davit <michel.davit@gmail.com>

--- core/src/main/scala/fr/davit/akka/http/metrics/core/MeterStage.scala
+++ core/src/main/scala/fr/davit/akka/http/metrics/core/MeterStage.scala
@@ -42,49 +42,52 @@ private[metrics] class MeterStage(metricsHandler: HttpMetricsHandler)
     val pending: mutable.Queue[Promise[HttpResponse]] = mutable.Queue.empty
 
     override def preStart(): Unit = {
-      super.preStart()
       metricsHandler.onConnection(completion.future)(materializer.executionContext)
     }
 
-    setHandler(
-      requestIn,
-      new InHandler {
-        override def onPush(): Unit = {
-          val request = grab(requestIn)
-          val promise = Promise[HttpResponse]()
-          metricsHandler.onRequest(request, promise.future)(materializer.executionContext)
-          pending.enqueue(promise)
-          push(requestOut, request)
-        }
+    override def postStop(): Unit = {
+      completion.success(Done)
+    }
+
+    val requestHandler = new InHandler with OutHandler {
+      override def onPush(): Unit = {
+        val request = grab(requestIn)
+        val promise = Promise[HttpResponse]()
+        metricsHandler.onRequest(request, promise.future)(materializer.executionContext)
+        pending.enqueue(promise)
+        push(requestOut, request)
       }
-    )
-    setHandler(requestOut, new OutHandler {
       override def onPull(): Unit = pull(requestIn)
-    })
-    setHandler(
-      responseIn,
-      new InHandler {
-        override def onPush(): Unit = {
-          val promise  = pending.dequeue()
-          val response = grab(responseIn)
-          promise.success(response)
-          push(responseOut, response)
-        }
 
-        override def onUpstreamFinish(): Unit = {
-          completion.success(Done)
-          super.onUpstreamFinish()
-        }
+      override def onUpstreamFinish(): Unit                   = complete(requestOut) // do not completeStage and flush stream
+      override def onUpstreamFailure(ex: Throwable): Unit     = fail(requestOut, ex)
+      override def onDownstreamFinish(cause: Throwable): Unit = cancel(requestIn)
+    }
 
-        override def onUpstreamFailure(ex: Throwable): Unit = {
-          completion.success(Done)
-          pending.foreach(_.failure(ex))
-          super.onUpstreamFailure(ex)
-        }
+    val responseHandler = new InHandler with OutHandler {
+      override def onPush(): Unit = {
+        val promise  = pending.dequeue()
+        val response = grab(responseIn)
+        promise.success(response)
+        push(responseOut, response)
       }
-    )
-    setHandler(responseOut, new OutHandler {
       override def onPull(): Unit = pull(responseIn)
-    })
+
+      override def onUpstreamFinish(): Unit = {
+        pending.foreach(_.failure(new IllegalStateException("Server stopped with pending requests")))
+        complete(responseOut)
+      }
+      override def onUpstreamFailure(ex: Throwable): Unit = {
+        pending.foreach(_.failure(ex))
+        fail(responseOut, ex)
+      }
+      override def onDownstreamFinish(cause: Throwable): Unit = {
+        pending.foreach(_.failure(cause))
+        cancel(responseIn)
+      }
+    }
+
+    setHandlers(requestIn, requestOut, requestHandler)
+    setHandlers(responseIn, responseOut, responseHandler)
   }
 }
---
+++ core/src/test/scala/fr/davit/akka/http/metrics/core/MeterStageSpec.scala
@@ -0,0 +1,203 @@
+/*
+ * Copyright 2019 Michel Davit
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package fr.davit.akka.http.metrics.core
+
+import akka.Done
+import akka.actor.ActorSystem
+import akka.http.scaladsl.model.{HttpRequest, HttpResponse}
+import akka.stream.ClosedShape
+import akka.stream.scaladsl.{GraphDSL, RunnableGraph}
+import akka.stream.testkit.scaladsl.{TestSink, TestSource}
+import akka.testkit.TestKit
+import org.scalamock.matchers.ArgCapture.CaptureOne
+import org.scalamock.scalatest.MockFactory
+import org.scalatest.concurrent.ScalaFutures
+import org.scalatest.flatspec.AnyFlatSpecLike
+import org.scalatest.matchers.should.Matchers
+
+import scala.concurrent.{ExecutionContext, Future}
+
+class MeterStageSpec
+    extends TestKit(ActorSystem("HttpMetricsSpec"))
+    with AnyFlatSpecLike
+    with Matchers
+    with MockFactory
+    with ScalaFutures {
+
+  trait Fixture {
+    val handler           = mock[HttpMetricsHandler]
+    val terminationFuture = CaptureOne[Future[Done]]()
+
+    (handler
+      .onConnection(_: Future[Done])(_: ExecutionContext))
+      .expects(capture(terminationFuture), *)
+      .returns((): Unit)
+
+    val (requestIn, requestOut, responseIn, responseOut) = RunnableGraph
+      .fromGraph(
+        GraphDSL.create(
+          TestSource.probe[HttpRequest],
+          TestSink.probe[HttpRequest],
+          TestSource.probe[HttpResponse],
+          TestSink.probe[HttpResponse]
+        )((_, _, _, _)) { implicit builder => (reqIn, reqOut, respIn, respOut) =>
+          import GraphDSL.Implicits._
+          val meter = builder.add(new MeterStage(handler))
+
+          reqIn ~> meter.in1
+          meter.out1 ~> reqOut
+          respIn ~> meter.in2
+          meter.out2 ~> respOut
+          ClosedShape
+        }
+      )
+      .run()
+
+    // simulate downstream demand
+    responseOut.request(1)
+    requestOut.request(1)
+  }
+
+  "MeterStage" should "call onConnection on materialization and complete the future once terminated" in new Fixture {
+    requestIn.sendComplete()
+    requestOut.expectComplete()
+
+    responseIn.sendComplete()
+    responseOut.expectComplete()
+
+    terminationFuture.value.futureValue shouldBe Done
+  }
+
+  it should "call onRequest wen request is offered" in new Fixture {
+    val request        = HttpRequest()
+    val responseFuture = CaptureOne[Future[HttpResponse]]()
+
+    (handler
+      .onRequest(_: HttpRequest, _: Future[HttpResponse])(_: ExecutionContext))
+      .expects(request, capture(responseFuture), *)
+      .returns((): Unit)
+
+    requestIn.sendNext(request)
+    requestOut.expectNext() shouldBe request
+
+    responseFuture.value.isCompleted shouldBe false
+
+    val response = HttpResponse()
+    responseIn.sendNext(response)
+    responseOut.expectNext() shouldBe response
+
+    responseFuture.value.futureValue shouldBe response
+  }
+
+  it should "flush the stream before stopping" in new Fixture {
+    val request = HttpRequest()
+    val actual  = CaptureOne[Future[HttpResponse]]()
+
+    (handler
+      .onRequest(_: HttpRequest, _: Future[HttpResponse])(_: ExecutionContext))
+      .expects(request, capture(actual), *)
+      .returns((): Unit)
+
+    requestIn.sendNext(request)
+    requestOut.expectNext() shouldBe request
+
+    actual.value.isCompleted shouldBe false
+
+    // close request side
+    requestIn.sendComplete()
+    requestOut.expectComplete()
+
+    // response should still be accepted
+    val response = HttpResponse()
+    responseIn.sendNext(response)
+    responseOut.expectNext() shouldBe response
+
+    actual.value.futureValue shouldBe response
+  }
+
+  it should "propagate error from request in" in new Fixture {
+    val request = HttpRequest()
+    val actual  = CaptureOne[Future[HttpResponse]]()
+
+    (handler
+      .onRequest(_: HttpRequest, _: Future[HttpResponse])(_: ExecutionContext))
+      .expects(request, capture(actual), *)
+      .returns((): Unit)
+
+    requestIn.sendNext(request)
+    requestOut.expectNext() shouldBe request
+
+    val error = new Exception("BOOM!")
+    requestIn.sendError(error)
+    requestOut.expectError(error)
+  }
+
+  it should "propagate error from request out" in new Fixture {
+    val request = HttpRequest()
+    val actual  = CaptureOne[Future[HttpResponse]]()
+
+    (handler
+      .onRequest(_: HttpRequest, _: Future[HttpResponse])(_: ExecutionContext))
+      .expects(request, capture(actual), *)
+      .returns((): Unit)
+
+    requestIn.sendNext(request)
+    requestOut.expectNext() shouldBe request
+
+    val error = new Exception("BOOM!")
+    requestOut.cancel(error)
+    requestIn.expectCancellation()
+  }
+
+  it should "propagate error from response in and complete pending" in new Fixture {
+    val request = HttpRequest()
+    val actual  = CaptureOne[Future[HttpResponse]]()
+
+    (handler
+      .onRequest(_: HttpRequest, _: Future[HttpResponse])(_: ExecutionContext))
+      .expects(request, capture(actual), *)
+      .returns((): Unit)
+
+    requestIn.sendNext(request)
+    requestOut.expectNext() shouldBe request
+
+    val error = new Exception("BOOM!")
+    responseIn.sendError(error)
+    responseOut.expectError(error)
+
+    actual.value.failed.futureValue shouldBe error
+  }
+
+  it should "propagate error from response out and complete pending" in new Fixture {
+    val request = HttpRequest()
+    val actual  = CaptureOne[Future[HttpResponse]]()
+
+    (handler
+      .onRequest(_: HttpRequest, _: Future[HttpResponse])(_: ExecutionContext))
+      .expects(request, capture(actual), *)
+      .returns((): Unit)
+
+    requestIn.sendNext(request)
+    requestOut.expectNext() shouldBe request
+
+    val error = new Exception("BOOM!")
+    responseOut.cancel(error)
+    responseIn.expectCancellation()
+
+    actual.value.failed.futureValue shouldBe error
+  }
+}
