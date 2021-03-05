541a5c3fb0fef771595c43523adade60d7cbbe96
Scala Steward <me@scala-steward.org>
2021-03-05 20:43:18 +0100

    Update metrics-core, metrics-json, ... to 4.1.18

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
     val akka                  = "2.6.12"
     val akkaHttp              = "10.2.3"
     val datadog               = "2.11.0"
-    val dropwizard            = "4.1.17"
+    val dropwizard            = "4.1.18"
     val dropwizardV5          = "5.0.0"
     val enumeratum            = "1.6.1"
     val logback               = "1.2.3"
