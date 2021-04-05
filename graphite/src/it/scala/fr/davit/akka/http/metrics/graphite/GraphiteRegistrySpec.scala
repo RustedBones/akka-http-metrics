5e0d7df5d9228e7aa93b85ae88d1d5a6496a4273
Michel Davit <michel.davit@gmail.com>
2021-04-05 18:30:20 +0200

    Fix exhaustive match

--- graphite/src/it/scala/fr/davit/akka/http/metrics/graphite/GraphiteRegistrySpec.scala
+++ graphite/src/it/scala/fr/davit/akka/http/metrics/graphite/GraphiteRegistrySpec.scala
@@ -45,13 +45,14 @@ class GraphiteRegistrySpec
     carbon.send(IO(Tcp), Tcp.Bind(carbon.ref, new InetSocketAddress(0)))
     val port   = carbon.expectMsgType[Tcp.Bound].localAddress.getPort
     val socket = carbon.sender()
-    carbon.setAutoPilot(
-      (sender: ActorRef, msg: Any) =>
-        msg match {
-          case _: Tcp.Connected =>
-            sender ! Tcp.Register(handler.ref)
-            TestActor.KeepRunning
-        }
+    carbon.setAutoPilot((sender: ActorRef, msg: Any) =>
+      msg match {
+        case _: Tcp.Connected =>
+          sender ! Tcp.Register(handler.ref)
+          TestActor.KeepRunning
+        case _ =>
+          throw new Exception(s"Unexpected message $msg")
+      }
     )
 
     val client = new CarbonClient("localhost", port) {
