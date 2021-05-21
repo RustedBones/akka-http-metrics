1c2eb333dcf6e7f5c0a2ba874caa56ad8b3c7127
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2021-05-21 16:03:25 +0200

    Update scala-collection-compat to 2.4.4 (#135)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -11,7 +11,7 @@ object Dependencies {
     val enumeratum            = "1.6.1"
     val logback               = "1.2.3"
     val prometheus            = "0.10.0"
-    val scalaCollectionCompat = "2.4.3"
+    val scalaCollectionCompat = "2.4.4"
     val scalaLogging          = "3.9.3"
     val scalaMock             = "5.1.0"
     val scalaTest             = "3.2.9"
