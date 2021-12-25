69ddf13b3b3013ef18491f2d732b97a812892910
Scala Steward <me@scala-steward.org>
2021-12-26 00:58:51 +0100

    Update scalamock to 5.2.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -13,7 +13,7 @@ object Dependencies {
     val prometheus            = "0.12.0"
     val scalaCollectionCompat = "2.5.0"
     val scalaLogging          = "3.9.4"
-    val scalaMock             = "5.1.0"
+    val scalaMock             = "5.2.0"
     val scalaTest             = "3.2.10"
   }
 
