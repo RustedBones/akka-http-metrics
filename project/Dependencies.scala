1d22672f5fac9ae1f44d2bc87dba648ec634f4d3
Michel Davit <michel@davit.fr>
2022-04-02 20:05:11 +0200

    Respect scala casing in Dependencies (#247)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,49 +3,49 @@ import sbt._
 object Dependencies {
 
   object Versions {
-    val akka                  = "2.6.19"
-    val akkaHttp              = "10.2.9"
-    val datadog               = "4.0.0"
-    val dropwizard            = "4.2.9"
-    val dropwizardV5          = "5.0.0"
-    val enumeratum            = "1.7.0"
-    val logback               = "1.2.11"
-    val prometheus            = "0.15.0"
-    val scalaCollectionCompat = "2.7.0"
-    val scalaLogging          = "3.9.4"
-    val scalaMock             = "5.2.0"
-    val scalaTest             = "3.2.11"
+    val Akka                  = "2.6.19"
+    val AkkaHttp              = "10.2.9"
+    val Datadog               = "4.0.0"
+    val Dropwizard            = "4.2.9"
+    val DropwizardV5          = "5.0.0"
+    val Enumeratum            = "1.7.0"
+    val Logback               = "1.2.11"
+    val Prometheus            = "0.15.0"
+    val ScalaCollectionCompat = "2.7.0"
+    val ScalaLogging          = "3.9.4"
+    val ScalaMock             = "5.2.0"
+    val ScalaTest             = "3.2.11"
   }
 
-  val akkaHttp         = "com.typesafe.akka"          %% "akka-http"             % Versions.akkaHttp
-  val datadog          = "com.datadoghq"               % "java-dogstatsd-client" % Versions.datadog
-  val dropwizardCore   = "io.dropwizard.metrics"       % "metrics-core"          % Versions.dropwizard
-  val dropwizardJson   = "io.dropwizard.metrics"       % "metrics-json"          % Versions.dropwizard
-  val dropwizardV5Core = "io.dropwizard.metrics5"      % "metrics-core"          % Versions.dropwizardV5
-  val dropwizardV5Json = "io.dropwizard.metrics5"      % "metrics-json"          % Versions.dropwizardV5
-  val enumeratum       = "com.beachape"               %% "enumeratum"            % Versions.enumeratum
-  val prometheusCommon = "io.prometheus"               % "simpleclient_common"   % Versions.prometheus
-  val scalaLogging     = "com.typesafe.scala-logging" %% "scala-logging"         % Versions.scalaLogging
+  val AkkaHttp         = "com.typesafe.akka"          %% "akka-http"             % Versions.AkkaHttp
+  val Datadog          = "com.datadoghq"               % "java-dogstatsd-client" % Versions.Datadog
+  val DropwizardCore   = "io.dropwizard.metrics"       % "metrics-core"          % Versions.Dropwizard
+  val DropwizardJson   = "io.dropwizard.metrics"       % "metrics-json"          % Versions.Dropwizard
+  val DropwizardV5Core = "io.dropwizard.metrics5"      % "metrics-core"          % Versions.DropwizardV5
+  val DropwizardV5Json = "io.dropwizard.metrics5"      % "metrics-json"          % Versions.DropwizardV5
+  val Enumeratum       = "com.beachape"               %% "enumeratum"            % Versions.Enumeratum
+  val PrometheusCommon = "io.prometheus"               % "simpleclient_common"   % Versions.Prometheus
+  val ScalaLogging     = "com.typesafe.scala-logging" %% "scala-logging"         % Versions.ScalaLogging
 
   object Provided {
-    val akkaStream = "com.typesafe.akka" %% "akka-stream" % Versions.akka % "provided"
+    val AkkaStream = "com.typesafe.akka" %% "akka-stream" % Versions.Akka % "provided"
   }
 
   object Test {
-    val akkaHttpJson      = "com.typesafe.akka"     %% "akka-http-spray-json" % Versions.akkaHttp     % "it,test"
-    val akkaHttpTestkit   = "com.typesafe.akka"     %% "akka-http-testkit"    % Versions.akkaHttp     % "it,test"
-    val akkaSlf4j         = "com.typesafe.akka"     %% "akka-slf4j"           % Versions.akka         % "it,test"
-    val akkaStreamTestkit = "com.typesafe.akka"     %% "akka-stream-testkit"  % Versions.akka         % "it,test"
-    val akkaTestkit       = "com.typesafe.akka"     %% "akka-testkit"         % Versions.akka         % "it,test"
-    val dropwizardJvm     = "io.dropwizard.metrics"  % "metrics-jvm"          % Versions.dropwizard   % "it,test"
-    val dropwizardV5Jvm   = "io.dropwizard.metrics5" % "metrics-jvm"          % Versions.dropwizardV5 % "it,test"
-    val logback           = "ch.qos.logback"         % "logback-classic"      % Versions.logback      % "it,test"
-    val prometheusHotspot = "io.prometheus"          % "simpleclient_hotspot" % Versions.prometheus   % "it,test"
+    val AkkaHttpJson      = "com.typesafe.akka"     %% "akka-http-spray-json" % Versions.AkkaHttp     % "it,test"
+    val AkkaHttpTestkit   = "com.typesafe.akka"     %% "akka-http-testkit"    % Versions.AkkaHttp     % "it,test"
+    val AkkaSlf4j         = "com.typesafe.akka"     %% "akka-slf4j"           % Versions.Akka         % "it,test"
+    val AkkaStreamTestkit = "com.typesafe.akka"     %% "akka-stream-testkit"  % Versions.Akka         % "it,test"
+    val AkkaTestkit       = "com.typesafe.akka"     %% "akka-testkit"         % Versions.Akka         % "it,test"
+    val DropwizardJvm     = "io.dropwizard.metrics"  % "metrics-jvm"          % Versions.Dropwizard   % "it,test"
+    val DropwizardV5Jvm   = "io.dropwizard.metrics5" % "metrics-jvm"          % Versions.DropwizardV5 % "it,test"
+    val Logback           = "ch.qos.logback"         % "logback-classic"      % Versions.Logback      % "it,test"
+    val PrometheusHotspot = "io.prometheus"          % "simpleclient_hotspot" % Versions.Prometheus   % "it,test"
 
-    val scalaCollectionCompat =
-      "org.scala-lang.modules" %% "scala-collection-compat" % Versions.scalaCollectionCompat % "it,test"
-    val scalaMock = "org.scalamock" %% "scalamock" % Versions.scalaMock % "it,test"
-    val scalaTest = "org.scalatest" %% "scalatest" % Versions.scalaTest % "it,test"
+    val ScalaCollectionCompat =
+      "org.scala-lang.modules" %% "scala-collection-compat" % Versions.ScalaCollectionCompat % "it,test"
+    val ScalaMock = "org.scalamock" %% "scalamock" % Versions.ScalaMock % "it,test"
+    val ScalaTest = "org.scalatest" %% "scalatest" % Versions.ScalaTest % "it,test"
   }
 
 }
